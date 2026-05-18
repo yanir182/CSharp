05f31d00424a1fbadb3d8307320062ab5c95c5f8
yanir182 <a_kuzminov890@mail.ru>
2026-05-18 19:10:38 +0300

    С#

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        TcpClient client = new TcpClient("192.168.0.3", 5000);
+        NetworkStream stream = client.GetStream();
+
+        new Thread(() =>
+        {
+            byte[] buffer = new byte[2048];
+            while (true)
+            {
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead <= 0) break;
+                Console.WriteLine(Encoding.UTF8.GetString(buffer, 0, bytesRead));
+            }
+        }).Start();
+    }
+}
