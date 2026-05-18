cb6542e3a2318f46e99fbff1c89fc9b9b3868680
yanir182 <a_kuzminov890@mail.ru>
2026-05-18 19:09:56 +0300

    С#

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {  
+                TcpClient client = new TcpClient();
+                client.Connect("192.168.0.3", 5000);
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine("Введите сообщение:");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Ответ от сервера - " + response);
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
